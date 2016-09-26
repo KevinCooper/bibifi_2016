3026e8b1e6a4fa973938d244d4ae62118f916b28
KevinCooper <boo2atfoo@gmail.com>
2016-09-25 22:24:36 -0400

    Should include the custom error types.  The proposed data format is included as well.

---
+++ ver1/interpreter_scripts/errors.py
@@ -0,0 +1,27 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class SecurityError(Error):
+    """Exception raised for a security error.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+class FailError(Error):
+    """Exception raised for a security error.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
